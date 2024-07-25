b0355f86b223f809cec3e94d2960add2619141bb
aleksulsk <aleksulsk@yandex.ru>
2024-07-25 13:00:58 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,4 @@
+first_name = "12"
+second_name = "1.5"
+last_name = "Python,"
+print("Курс:", last_name, "всего задач:", first_name, ",", "затрачено часов:", second_name, ",",  "среднее время выполнения", 1.5/12, "часа.")
