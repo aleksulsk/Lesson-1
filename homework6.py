605f7dba324fd4d688e42d5709ff7f8e6ba6c88a
aleksulsk <aleksulsk@yandex.ru>
2024-07-27 13:35:17 +0400

    Create homework6.py

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {"Ivan": 1987, "Vasya": 2002, "Kolya":1999}
+print(my_dict)
+print(my_dict.get("Ivan"))
+print(my_dict.get("Alex"))
+my_dict.update({"Zhenya": 1985, "Kostya": 2005})
+my_dict.pop("Ivan")
+print(my_dict)
+
+my_set = {1, 2, 1, 2, "Hello", True, "Hello"}
+print(my_set)
+my_set.add(3)
+my_set.add(4)
+my_set.discard(2)
+print(my_set)
