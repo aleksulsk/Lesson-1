5181d53816f2c40093befa370f6e0e72f6649443
aleksulsk <aleksulsk@yandex.ru>
2024-07-27 19:42:38 +0400

    Create module1hard.py

---
+++ module1hard.py
@@ -0,0 +1,2 @@
+students = {'Johnny': sum((5, 3, 3, 5, 4)) / len((5, 3, 3, 5, 4)), 'Bilbo': sum((2, 2, 2, 3)) / len((2, 2, 2, 3)), "Steve": sum((4, 5, 5, 2)) / len(( 4, 5, 5, 2)), 'Khendrik': sum((4, 4, 3)) / len(( 4, 4, 3)), 'Aaron': sum((5, 5, 5, 4, 5)) / len((5, 5, 5, 4, 5))}
+print(students)
