877f586e22a6504fe013ee0c11d24bc98fa99904
aleksulsk <aleksulsk@yandex.ru>
2024-08-07 14:03:58 +0400

    Create module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,29 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls = calls + 1
+    return
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.upper() == string.upper():
+            return True
+        elif i is not list_to_search:
+            a = False
+    return a
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
