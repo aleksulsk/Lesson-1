21b95f68b5a2a1d3d4addfee7de2805cc4a885e2
aleksulsk <aleksulsk@yandex.ru>
2024-08-08 12:50:41 +0400

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,17 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    variants = (".com", ".ru", ".net")
+    if not (sender.lower().endswith(variants) and recipient.lower().endswith(variants)
+            and "@" in recipient and "@" in sender):
+        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, "на адрес", recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
